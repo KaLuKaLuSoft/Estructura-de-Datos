0099445be4dc3a69fc5ba17426e18270a884b810
KaLuKaLuSoft <diego_2004_13@hotmail.com>
2021-07-16 16:39:28 -0400

    Actualización

--- Principal.Designer.cs
+++ Principal.Designer.cs
@@ -49,6 +49,9 @@ namespace Estructura_de_Datos
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_Ordenado
@@ -165,27 +168,27 @@ namespace Estructura_de_Datos
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(257, 65);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.Size = new System.Drawing.Size(40, 17);
             this.label2.TabIndex = 11;
-            this.label2.Text = "2";
+            this.label2.Text = "2 OK";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(372, 65);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.Size = new System.Drawing.Size(40, 17);
             this.label3.TabIndex = 12;
-            this.label3.Text = "3";
+            this.label3.Text = "3 OK";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(511, 65);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.Size = new System.Drawing.Size(40, 17);
             this.label4.TabIndex = 13;
-            this.label4.Text = "4";
+            this.label4.Text = "4 OK";
             // 
             // label5
             // 
@@ -210,18 +213,18 @@ namespace Estructura_de_Datos
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(257, 213);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(16, 17);
+            this.label7.Size = new System.Drawing.Size(40, 17);
             this.label7.TabIndex = 16;
-            this.label7.Text = "7";
+            this.label7.Text = "7 OK";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(372, 213);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(16, 17);
+            this.label8.Size = new System.Drawing.Size(20, 17);
             this.label8.TabIndex = 17;
-            this.label8.Text = "8";
+            this.label8.Text = "8 ";
             // 
             // label9
             // 
@@ -241,11 +244,41 @@ namespace Estructura_de_Datos
             this.label10.TabIndex = 19;
             this.label10.Text = "10";
             // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(0, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(54, 17);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(239, 410);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(360, 17);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "8 no me convences que este bien y el 9 no me funciona";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(323, 375);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(125, 17);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "1-5-6-10 me faltan";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -295,5 +328,8 @@ namespace Estructura_de_Datos
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
     }
 }
